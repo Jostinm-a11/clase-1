4bdce70f2974320784f5d7dbb6cf76130330948a
Jostinm-a11 <nana1406.m@gmail.com>
2025-01-23 17:42:07 -0400

    Delete codigos de codeblocks directory

--- codigos de codeblocks/Untitled1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(void)
-{
-    int A, B, C ,D;
-    printf("ingrese cuatro datos de tipo entero");
-    scanf("%d %d %d", &B, &C, &D);
-    printf("\n %d %d %d %d", D, C, B, A);
-}
